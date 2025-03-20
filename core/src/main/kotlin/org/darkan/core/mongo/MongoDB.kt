8f041e2b9870fdd84aaac6e7c5088b19665b21d5
Trenton Kress <trenton.kress@gmail.com>
2025-03-20 01:20:26 -0700

    chore: organize some mongodb init garbage

--- core/src/main/kotlin/org/darkan/core/mongo/MongoDB.kt
+++ core/src/main/kotlin/org/darkan/core/mongo/MongoDB.kt
@@ -3,17 +3,22 @@ package org.darkan.core.mongo
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.kotlin.client.coroutine.MongoClient
+import org.bson.codecs.configuration.CodecRegistries
+import org.bson.codecs.pojo.PojoCodecProvider
 import org.darkan.core.EnvVars
 
 object MongoDB {
     private val settings = MongoClientSettings.builder()
         .applyConnectionString(ConnectionString(EnvVars.mongoUri))
-        .codecRegistry(Codecs.codecRegistry)
+        .codecRegistry(CodecRegistries.fromRegistries(
+            MongoClientSettings.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(PojoCodecProvider.builder()
+                .automatic(true)
+                .build())
+        ))
         .build()
 
     val client = MongoClient.create(settings)
     val database = client.getDatabase(ConnectionString(EnvVars.mongoUri).database ?: "darkan-server")
-
-    internal inline fun <reified T : Any> collection(name: String) = database.getCollection(name, T::class.java)
 }
 
