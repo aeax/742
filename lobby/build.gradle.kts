78b80dc1c65cb0cbe38e26aef4a68736282444ec
Trenton Kress <trenton.kress@gmail.com>
2025-04-04 08:52:06 -0700

    chore: bump kotlin and gradle to latest

--- lobby/build.gradle.kts
+++ lobby/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     application
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 application {
